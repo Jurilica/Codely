92837e4f97a1bc039af11db78f647c130b441138
Jurilica <80757501+Jurilica@users.noreply.github.com>
2023-08-04 14:09:54 +0200

    Filter archived entites

--- Codely.Core/Data/CodelyContext.cs
+++ Codely.Core/Data/CodelyContext.cs
@@ -1,4 +1,6 @@
-﻿using Codely.Core.Data.Entities;
+﻿using System.Linq.Expressions;
+using Codely.Core.Data.Entities;
+using Codely.Core.Data.Entities.Base;
 using Microsoft.EntityFrameworkCore;
 
 namespace Codely.Core.Data;
@@ -25,4 +27,37 @@ public class CodelyContext : DbContext
     public DbSet<TestCase> TestCases => Set<TestCase>();
 
     public DbSet<User> Users => Set<User>();
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var softDeleteEntities = typeof(BaseEntity).Assembly.GetTypes()
+            .Where(x => typeof(BaseEntity).IsAssignableFrom(x))
+            .Where(x => x.IsClass)
+            .Where(x => !x.IsAbstract);
+        
+        foreach (var softDeleteEntity in softDeleteEntities)
+        {
+            modelBuilder.Entity(softDeleteEntity)
+                .HasQueryFilter(GenerateQueryFilterLambdaExpression(softDeleteEntity));
+        }
+
+        base.OnModelCreating(modelBuilder);
+    }
+    
+    private static LambdaExpression GenerateQueryFilterLambdaExpression(Type type)
+    {
+        // we generate:  x => x.ArchivedTime == null
+        // x =>
+        var parameter = Expression.Parameter(type, "x");
+        // null
+        var falseConstant = Expression.Constant(null);
+        // x.ArchiveDate
+        var propertyAccess = Expression.PropertyOrField(parameter, nameof(BaseEntity.Created));
+        // e.ArchiveDate == null
+        var equalExpression = Expression.Equal(propertyAccess, falseConstant);
+        // x => e.ArchiveDate == null
+        var lambda = Expression.Lambda(equalExpression, parameter);
+        
+        return lambda;
+    }
 }
