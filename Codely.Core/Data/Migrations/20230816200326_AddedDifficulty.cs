099c5de179a4cff6250369870c3c488d17686c2f
Jurilica <80757501+Jurilica@users.noreply.github.com>
2023-08-16 22:04:02 +0200

    New migration

---
+++ Codely.Core/Data/Migrations/20230816200326_AddedDifficulty.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Codely.Core.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDifficulty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "difficulty",
+                table: "problems",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "difficulty",
+                table: "problems");
+        }
+    }
+}
