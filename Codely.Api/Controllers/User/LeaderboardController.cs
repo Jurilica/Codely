6f1fb8a9bff40bb8bca7575cfe47dfab29cb43ad
Jurilica <80757501+Jurilica@users.noreply.github.com>
2023-08-18 23:46:30 +0200

    Added leaderboard

---
+++ Codely.Api/Controllers/User/LeaderboardController.cs
@@ -0,0 +1,31 @@
+﻿using Codely.Api.Attributes;
+using Codely.Api.Constants;
+using Codely.Core.Handlers.User.Leaderboard;
+using Codely.Core.Handlers.User.Submission;
+using Codely.Core.Types.Enums;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codely.Api.Controllers.User;
+
+[Route("user/leaderboard")]
+[ApiController]
+[ApiExplorerSettings(GroupName = SwaggerConstants.User)]
+// [RoleAuthorize(Role.User)]
+public class LeaderboardController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public LeaderboardController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    [HttpGet]
+    [AllowAnonymous]
+    public async Task<GetLeaderboardResponse> GetSubmissions()
+    {
+        return await _mediator.Send(new GetLeaderboardRequest());
+    }
+}
---
+++ Codely.Core/Handlers/User/Leaderboard/GetLeaderboardQuery.cs
@@ -0,0 +1,97 @@
+﻿using Codely.Core.Data;
+using Codely.Core.Types.Enums;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Codely.Core.Handlers.User.Leaderboard;
+
+public sealed class GetLeaderboardQuery : IRequestHandler<GetLeaderboardRequest, GetLeaderboardResponse>
+{
+    private readonly CodelyContext _context;
+
+    public GetLeaderboardQuery(CodelyContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<GetLeaderboardResponse> Handle(GetLeaderboardRequest request, CancellationToken cancellationToken)
+    {
+        var lastSubmissionsPerUser = await _context.Submissions
+            .AsNoTracking()
+            .Where(x => x.Problem.Status == ProblemStatus.Published)
+            .Select(x =>
+                new
+                {
+                    x.User.Username,
+                    x.Problem.Difficulty,
+                    x.SubmissionStatus,
+                    x.ProblemId,
+                    x.Created
+                })
+            .GroupBy(x => new {x.Username, x.ProblemId})
+            .Select(x =>
+                new
+                {
+                    SubmissionData = x
+                        .OrderByDescending(y => y.Created)
+                        .First()
+                })
+            .ToListAsync(cancellationToken);
+
+        var leaderboardData = lastSubmissionsPerUser
+            .Where(x => x.SubmissionData.SubmissionStatus == SubmissionStatus.Succeeded)
+            .GroupBy(x => x.SubmissionData.Username)
+            .Select(x =>
+                new
+                {
+                    Username = x.Key,
+                    EasyProblemsSolved = x
+                        .Where(y => y.SubmissionData.Difficulty == ProblemDifficulty.Easy)
+                        .Count(),
+                    MediumProblemsSolved = x
+                        .Where(y => y.SubmissionData.Difficulty == ProblemDifficulty.Medium)
+                        .Count(),
+                    HardProblemsSolved = x
+                        .Where(y => y.SubmissionData.Difficulty == ProblemDifficulty.Hard)
+                        .Count()
+                })
+            .Select(x => 
+                new LeaderboardData
+                {
+                    Username = x.Username,
+                    EasyProblemsSolved = x.EasyProblemsSolved,
+                    MediumProblemsSolved = x.MediumProblemsSolved,
+                    HardProblemsSolved = x.HardProblemsSolved,
+                    Points = x.EasyProblemsSolved + x.MediumProblemsSolved * 2 + x.HardProblemsSolved * 3
+                })
+            .ToList();
+
+        return new GetLeaderboardResponse
+        {
+            Leaderboard = leaderboardData
+        };
+    }
+}
+
+public sealed class GetLeaderboardRequest : IRequest<GetLeaderboardResponse>
+{
+
+}
+
+public sealed class GetLeaderboardResponse
+{
+    public required List<LeaderboardData> Leaderboard { get; init; }
+}
+
+public sealed class LeaderboardData
+{
+    public required string Username { get; init; }
+    
+    public required int Points { get; init; }
+    
+    public required int EasyProblemsSolved { get; init; }
+    
+    public required int MediumProblemsSolved { get; init; }
+    
+    public required int HardProblemsSolved { get; init; }
+}
